f5052dbbce9d3920f4311b8e0e3ad625b08c84b4
Gordon McCann <gmccann@fsu.edu>
2021-08-26 09:24:27 -0400

    Added (probable) Windows compatibility, needs testing to verify

--- premake5.lua
+++ premake5.lua
@@ -17,20 +17,45 @@ project "Mask"
 		"src/**.cxx"
 	}
 
-	includedirs {
-		"include",
-		"./"
-	}
-
-	buildoptions {
-		"`root-config --cflags`"
-	}
-
-	linkoptions {
-		"`root-config --glibs`"
-	}
-
 	prebuildcommands {
 		"rootcint -f src/kinematics_dict.cxx include/Kinematics.h include/LinkDef_Kinematics.h",
 		"{MOVE} src/kinematics_dict_rdict.pcm bin/"
 	}
+
+	filter "system:windows"
+		systemversion "latest"
+		--ROOT cannot be located using the config tools, so we must query for the ROOTSYS env variable
+		--Have to use an if statement to hide this (@penguin for example doesn't have a ROOTSYS)
+		if os.host() == windows then
+			rootpath = os.getenv("ROOTSYS")
+
+			includedirs {
+				"include",
+				"./",
+				rootpath .. "include"
+			}
+
+			links {
+				rootpath .. "lib/**.lib"
+			}
+		end
+
+	filter "system:macosx or linux"
+		includedirs {
+			"include",
+			"./"
+		}
+
+		buildoptions {
+			"`root-config --cflags`"
+		}
+
+		linkoptions {
+			"`root-config --glibs`"
+		}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
