0b2f4f74ee4f47cc8383a39100c19513f16a906c
Gordon McCann <gmccann@fsu.edu>
2022-06-08 15:58:17 -0400

    Remove unecessary file check

--- src/MaskApp/main.cpp
+++ src/MaskApp/main.cpp
@@ -29,24 +29,6 @@ int main(int argc, char** argv) {
 	sw.Stop();
 	std::cout<<"Time elapsed(seconds): "<<sw.GetElapsedSeconds()<<std::endl;
 	
-	Mask::MaskFile input(calculator.GetOutputName(), Mask::MaskFile::FileType::read);
-	Mask::MaskFileData data;
-	Mask::MaskFileHeader header = input.ReadHeader();
-
-	std::cout<<"Header Found -- rxn type: "<<GetStringFromRxnType(header.rxn_type)<<" nsamples: "<<header.nsamples;
-	std::cout<<std::endl;
-
-	int counter=0;
-	while(!data.eof) {
-		data = input.ReadData();
-		for(unsigned int i=0; i<data.E.size(); i++)
-			counter++;
-			//std::cout<<"Data Found -- E: "<<data.E[i]<<" KE: "<<data.KE[i]<<" p: "<<data.p[i]<<" theta: "<<data.theta[i]<<" phi: "<<data.phi[i]<<std::endl;
-	}
-	std::cout<<"events found: "<<counter<<std::endl;
-	input.Close();
-	std::cout<<"File closed."<<std::endl;
-
 	return 0;
 
 }
